4c70d387ca84fa1333cdccc91677e43097069f75
saintjb <48727494+saintjb@users.noreply.github.com>
2020-05-24 17:10:04 +0300

    scroll list, remove elements with long press

--- App.js
+++ App.js
@@ -1,18 +1,37 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {useState} from 'react';
+import { StyleSheet, Text, View, FlatList } from 'react-native';
 import {Navbar} from './src/Navbar';
-import {AddTodo} from './src/AddTodo'
+import {AddTodo} from './src/AddTodo';
+import {Todo} from './src/Todo';
 
 export default function App() {
+  const [todos, setTodos] = useState([])
+  const addTodo = (title) => {
+
+    setTodos(prev => [...prev,{
+      id: Date.now().toString(),
+       title
+    }])
+  }
+
+  const removeTodo = id => {
+    setTodos(prev => prev.filter(todo => todo.id !== id))
+  }
+ 
+
   return (
 
     <View>
       <Navbar title='Todo App'/>
       <View style = {styles.container}>
-        <AddTodo/>
+        <AddTodo onSubmit={addTodo}/>
+        <FlatList
+        keyExtractor={item => item.id}
+        data={todos}
+        renderItem={({item}) => 
+          <Todo todo={item} onRemove={removeTodo}/>}
+        />
       </View>
- 
-
     </View>
 
   );
--- src/AddTodo.js
+++ src/AddTodo.js
@@ -1,11 +1,27 @@
-import React from 'react';
-import {View, StyleSheet, TextInput, Button} from 'react-native';
+import React, {useState} from 'react';
+import {View, StyleSheet, TextInput, Button, Alert} from 'react-native';
 
-export const AddTodo = (props) => {
+export const AddTodo = ({ onSubmit }) => {
+
+    const [value, setValue] = useState('')
+
+    const pressHandler = () => {
+        if (value.trim()){
+            onSubmit(value)
+            setValue('')
+        }
+        else {
+            Alert.alert("Name of the deal can't be an empty")
+        }
+    }
     return(
         <View style={styles.block}>
-            <TextInput style= {styles.input}/>
-            <Button title='Add'/>
+            <TextInput style= {styles.input}
+            onChangeText={setValue}
+            value={value}
+            placeholder='Enter your deal...'
+            autoCorrect={false}/>
+            <Button title='Add' onPress={pressHandler}/>
         </View>
     );
 };
@@ -15,6 +31,7 @@ const styles = StyleSheet.create({
         flexDirection: 'row',
         justifyContent: 'space-between',
         alignItems: 'center',
+        marginBottom: 15,
     },
     input:{
         width:'70%',
